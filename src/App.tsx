85d2d95eba5f337146b407a9363558e7583a62e3
alexfcode <alexf@tut.by>
2025-05-04 16:41:32 +0300

    microtask is finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { NewComponent } from './components/NewComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export type FilterType = "all" | "dollar" | "ruble"
+
+export type MoneyType = {
+  banknote: string
+  nominal: number
+  number: string
 }
 
-export default App;
+export const App = () => {
+
+  const [money, setMoney] = useState([
+    { banknote: "dollar", nominal: 100, number: "a123456789" },
+    { banknote: "dollar", nominal: 50, number: "b123456789" },
+    { banknote: "ruble", nominal: 100, number: "c123456789" },
+    { banknote: "dollar", nominal: 100, number: "d123456789" },
+    { banknote: "dollar", nominal: 50, number: "e123456789" },
+    { banknote: "ruble", nominal: 100, number: "f123456789" },
+    { banknote: "dollar", nominal: 50, number: "j123456789" },
+    { banknote: "ruble", nominal: 50, number: "h123456789" }
+  ])
+
+
+  const [filter, seFilter] = useState("all")
+
+  let filteredMoney = money
+
+  if (filter === "ruble") {
+    filteredMoney = money.filter((mon) => mon.banknote === "ruble")
+  }
+  if (filter === "dollar") {
+    filteredMoney = money.filter((mon) => mon.banknote === "dollar")
+  }
+
+  const onClickFilterHandler = (buttonName: FilterType) => {
+    seFilter(buttonName)
+  }
+
+
+  return (
+    <div className='App'>
+      <NewComponent filteredMoney={filteredMoney} onClickFilterHandler={onClickFilterHandler}/>
+    </div>
+  )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+type ButtonPropsType = {
+    name: string
+    callback: () => void
+}
+
+export const Button = ({ name, callback }: ButtonPropsType) => {
+
+    const onClickHandler = () => {
+        callback()
+
+    }
+
+    return (
+        <button onClick={onClickHandler}>{name}</button>
+    )
+}
